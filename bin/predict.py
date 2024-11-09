b80cce66cde6a304d4ec4ba92477dd7bc6669e29
mntijn <montijn02@gmail.com>
2024-11-09 14:03:22 +0100

    use GPU

--- bin/predict.py
+++ bin/predict.py
@@ -6,6 +6,17 @@
 #       indir=<path to input data> \
 #       outdir=<where to store predicts>
 
+from saicinpainting.utils import register_debug_signal_handlers
+from saicinpainting.training.trainers import load_checkpoint
+from saicinpainting.training.data.datasets import make_default_val_dataset
+from torch.utils.data._utils.collate import default_collate
+from omegaconf import OmegaConf
+import yaml
+import tqdm
+import torch
+import numpy as np
+import hydra
+import cv2
 import logging
 import os
 import sys
@@ -19,18 +30,6 @@ os.environ['MKL_NUM_THREADS'] = '1'
 os.environ['VECLIB_MAXIMUM_THREADS'] = '1'
 os.environ['NUMEXPR_NUM_THREADS'] = '1'
 
-import cv2
-import hydra
-import numpy as np
-import torch
-import tqdm
-import yaml
-from omegaconf import OmegaConf
-from torch.utils.data._utils.collate import default_collate
-
-from saicinpainting.training.data.datasets import make_default_val_dataset
-from saicinpainting.training.trainers import load_checkpoint
-from saicinpainting.utils import register_debug_signal_handlers
 
 LOGGER = logging.getLogger(__name__)
 
@@ -39,23 +38,27 @@ LOGGER = logging.getLogger(__name__)
 def main(predict_config: OmegaConf):
     try:
         if sys.platform != 'win32':
-            register_debug_signal_handlers()  # kill -10 <pid> will result in traceback dumped into log
+            # kill -10 <pid> will result in traceback dumped into log
+            register_debug_signal_handlers()
 
-        device = torch.device("cpu")
+        device_name = "cuda" if torch.cuda.is_available() else "cpu"
+        device = torch.device(device_name)
 
-        train_config_path = os.path.join(predict_config.model.path, 'config.yaml')
+        train_config_path = os.path.join(
+            predict_config.model.path, 'config.yaml')
         with open(train_config_path, 'r') as f:
             train_config = OmegaConf.create(yaml.safe_load(f))
-        
+
         train_config.training_model.predict_only = True
         train_config.visualizer.kind = 'noop'
 
         out_ext = predict_config.get('out_ext', '.png')
 
-        checkpoint_path = os.path.join(predict_config.model.path, 
-                                       'models', 
+        checkpoint_path = os.path.join(predict_config.model.path,
+                                       'models',
                                        predict_config.model.checkpoint)
-        model = load_checkpoint(train_config, checkpoint_path, strict=False, map_location='cpu')
+        model = load_checkpoint(
+            train_config, checkpoint_path, strict=False, map_location=device_name)
         model.freeze()
         if not predict_config.get('refine', False):
             model.to(device)
@@ -63,12 +66,14 @@ def main(predict_config: OmegaConf):
         if not predict_config.indir.endswith('/'):
             predict_config.indir += '/'
 
-        dataset = make_default_val_dataset(predict_config.indir, **predict_config.dataset)
+        dataset = make_default_val_dataset(
+            predict_config.indir, **predict_config.dataset)
         for img_i in tqdm.trange(len(dataset)):
             mask_fname = dataset.mask_filenames[img_i]
             cur_out_fname = os.path.join(
-                predict_config.outdir, 
-                os.path.splitext(mask_fname[len(predict_config.indir):])[0] + out_ext
+                predict_config.outdir,
+                os.path.splitext(mask_fname[len(predict_config.indir):])[
+                    0] + out_ext
             )
             os.makedirs(os.path.dirname(cur_out_fname), exist_ok=True)
             batch = default_collate([dataset[img_i]])
@@ -76,14 +81,16 @@ def main(predict_config: OmegaConf):
                 assert 'unpad_to_size' in batch, "Unpadded size is required for the refinement"
                 # image unpadding is taken care of in the refiner, so that output image
                 # is same size as the input image
-                cur_res = refine_predict(batch, model, **predict_config.refiner)
-                cur_res = cur_res[0].permute(1,2,0).detach().cpu().numpy()
+                cur_res = refine_predict(
+                    batch, model, **predict_config.refiner)
+                cur_res = cur_res[0].permute(1, 2, 0).detach().cpu().numpy()
             else:
                 with torch.no_grad():
                     batch = move_to_device(batch, device)
                     batch['mask'] = (batch['mask'] > 0) * 1
-                    batch = model(batch)                    
-                    cur_res = batch[predict_config.out_key][0].permute(1, 2, 0).detach().cpu().numpy()
+                    batch = model(batch)
+                    cur_res = batch[predict_config.out_key][0].permute(
+                        1, 2, 0).detach().cpu().numpy()
                     unpad_to_size = batch.get('unpad_to_size', None)
                     if unpad_to_size is not None:
                         orig_height, orig_width = unpad_to_size
@@ -96,7 +103,8 @@ def main(predict_config: OmegaConf):
     except KeyboardInterrupt:
         LOGGER.warning('Interrupted by user')
     except Exception as ex:
-        LOGGER.critical(f'Prediction failed due to {ex}:\n{traceback.format_exc()}')
+        LOGGER.critical(
+            f'Prediction failed due to {ex}:\n{traceback.format_exc()}')
         sys.exit(1)
 
 
